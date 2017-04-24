a11d79943f9b1778104c7198d2be2ebccfef6dee
Chris Want <cjwant@gmail.com>
2017-04-24 22:17:30 +0000

    Get docker-compose to "work" with U of A avalon, in particular with Solr and Fedora.

--- avalon/rails_init.sh
+++ avalon/rails_init.sh
@@ -14,7 +14,7 @@ chown -R app /masterfiles
 cd /home/app/avalon
 
 su app
-BACKGROUND=yes QUEUE=* bundle exec rake resque:work
+# BACKGROUND=yes QUEUE=* bundle exec rake resque:work
 BACKGROUND=yes bundle exec rake environment resque:scheduler
 RAILS_ENV=production bundle exec rake db:migrate
 exit
