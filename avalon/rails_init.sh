638ee19e468e02a0c93ef530891a7f9b623576ac
Chris Want <cjwant@gmail.com>
2017-04-25 21:07:13 +0000

    Development env works with Fedora/solr.

--- avalon/rails_init.sh
+++ avalon/rails_init.sh
@@ -15,8 +15,8 @@ cd /home/app/avalon
 
 su app
 # BACKGROUND=yes QUEUE=* bundle exec rake resque:work
-RAILS_ENV=production bundle exec rake db:migrate
-RAILS_ENV=production bundle exec script/delayed_job -n2 start
+RAILS_ENV=development bundle exec rake db:migrate
+RAILS_ENV=development bundle exec script/delayed_job -n2 start
 exit
 
 cd public/assets/mediaelement_rails
